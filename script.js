144a62e0d8b8318e686193e710814be64f9d682d
Eduardo Nascimento Couto Luiz <24123950@cfp127aluno.local>
2025-03-18 16:55:21 -0300

    tela home finalizada

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 'use strict'
 
-async function loadGallery () {
-    const home = document.getElementById('home')
-    const url = 'https://api.sampleapis.com/movies/animation'
-    
-    const response = await  fetch(url)
-    const animacoes = await response.json()
 
-    animacoes.forEach (animacoes =>{
-        const img = document.createElement('img')
-        img.src = animacoes.posterURL
 
-        home.appendChild(img)
-    })
-    
+
+async function pesquisarFilmesPeloTema(){
+    const url = "https://api.sampleapis.com/movies/animation"
+
+    const response = await fetch(url)
+    const dados = await response.json()
+    return dados
 }
-loadGallery()
+
+function animation(){
+    const body = document.createElement('div')
+    const main = document.getElementById('main')
+    const titulo = document.createElement('h1')
+
+    main.innerHTML = ""
+
+    body.className = "tela"
+    body.style.backgroundColor = "green"
+
+    titulo.id = "titulo"
+    titulo.textContent = 'ANIMAÇÃO'
+
+    body.appendChild(titulo)
+
+    main.appendChild(body)
+
+    
+
+}
+
+document.getElementById('animacoes').addEventListener('click', animation)
